c4e81f59089ecdad6468de976c37ddb45f3bc29b
Vu Nguyen Anh Tuan <blapthec4tu5@gmail.com>
2026-06-01 08:43:12 +0700

    update

--- src/circuit/objects/meter/AmpereMeter.java
+++ src/circuit/objects/meter/AmpereMeter.java
@@ -4,6 +4,7 @@ import circuit.objects.components.impedant.Component;
 import circuit.objects.components.interfaces.TerminalTag;
 import circuit.objects.components.other.Terminal;
 import circuit.objects.components.other.Connection;
+import java.util.ArrayList;
 
 public class AmpereMeter implements TerminalTag {
 
@@ -16,16 +17,10 @@ public class AmpereMeter implements TerminalTag {
         this.terminal2 = new Terminal(this);
 
         terminal1.setInternallyConnectedTerminal(terminal2);
-        terminal2.setInternallyConnectedTerminal(terminal1);
     }
 
     public void calculateCurrent(float voltage1, float voltage2) {
 
-        Connection connection = terminal1.getWire();
-
-        TerminalTag prev = connection.getTerminal1() == terminal1 ? connection.getTerminal2().getParent()
-                : connection.getTerminal1().getParent();
-        this.current = prev.getCurrent();
     }
 
     public float getCurrent() {
